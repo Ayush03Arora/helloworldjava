e2fbbd31a81a66d02565503c04d3cab47181f48b
Ayush Arora <76896397+Ayush03Arora@users.noreply.github.com>
2023-01-19 14:14:42 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World Sample Code. a");
+    System.out.println("Hello World Sample Code xyz");
     }
 }
